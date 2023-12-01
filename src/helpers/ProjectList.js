39b4863d0ce5bfd249d7db613ff213b46f5c739b
Trayden <traybouc@hotmail.com>
2023-12-01 04:49:10 -0500

    add react stat calc

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,5 +1,6 @@
 import CivProject from "../assets/napoleon_img.png";
 import StatWebsiteProject from "../assets/stat_calculator.png";
+import StatWebsiteProjectReact from "../assets/stat_calculator_react.png";
 
 export const ProjectList = [
     {
@@ -10,11 +11,19 @@ export const ProjectList = [
         info: "Developed a statistical analysis website, facilitating simple presentation of complex data to users. Designed with a responsive and intuitive interface, utilizing CSS media queries."
     },
     {
+        name: "Statistical Analysis Tool: React",
+        image: StatWebsiteProjectReact,
+        skills: "HTML, CSS, Javascript, React",
+        githubLink: "https://github.com/traydenpoke/lol-item-analysis",
+        info: "Developed a statistical analysis website, facilitating simple presentation of complex data to users. Designed with a responsive and intuitive interface, utilizing CSS media queries. New and improved from old version, created with React."
+    },
+    {
         name: "Video Game Mod Creation",
         image: CivProject,
         skills: "XML, SQL, SQLite",
         githubLink: "https://github.com/traydenpoke/Napoleon-Mod",
         info: "Created custom gameplay mods for Civilization VI, gathering 11,000+ views and subscriptions. Developed through modifying existing SQLite databases and XML files, creating unique and balanced mechanics."
     }
+
 
 ];
