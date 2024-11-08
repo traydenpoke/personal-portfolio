e4ab1c4ac8fa63ea0f38369392318de920135e0d
Trayden <traybouc@hotmail.com>
2024-11-07 19:30:20 -0500

    Update experience and skills

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,31 +2,44 @@ import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
 
 function Experience() {
-    return (
-        <div className="experience">
-            <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="2017 - 2021"
-                    iconStyle={{ background: "#3e497a", color: "#fff" }}
-                    icon={<SchoolIcon />}>
-                    <h3 className="vertical-timeline-element-title"> Bluevale Collegiate Institute </h3>
-                    <p> Ontario Secondary School Diploma </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="2021 - Present"
-                    iconStyle={{ background: "#3e497a", color: "#fff" }}
-                    icon={<SchoolIcon />}>
-                    <h3 className="vertical-timeline-element-title"> Queen's University, Kingston, Ontario </h3>
-                    <h4 className="vertical-timeline-element-subtitle"> Bachelor of Computing (Honours)  </h4>
-                    <p>Relevant Coursework: Data Structures, Algorithms, System Level Programming, Object-Oriented Programming.</p>
-                </VerticalTimelineElement>
-            </VerticalTimeline>
-        </div >
-    );
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2017 - 2021"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> Bluevale Collegiate Institute </h3>
+          <p> Ontario Secondary School Diploma </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2021 - Present"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> Queen's University, Kingston, Ontario </h3>
+          <h4 className="vertical-timeline-element-subtitle"> Bachelor of Computing (Honours)  </h4>
+          <p>Relevant Coursework: Data Structures, Algorithms, System Level Programming, Object-Oriented Programming.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="May 2024 - Present"
+          iconStyle={{ background: "rgb(33, 130, 243", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> Safety Power Inc., Mississauga, Ontario </h3>
+          <h4 className="vertical-timeline-element-subtitle"> Assistant Software Designer </h4>
+          <p>Assisting senior engineers and developers in developing environmental control technology to reduce emissions for Fortune 500 clients. Designing and optimizing full-stack software across Python, JavaScript, Linux, and embedded systems to enhance automation and improve performance.</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div >
+  );
 }
 
 export default Experience;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,50 +6,56 @@ import '../styles/Home.css';
 
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="about">
-                <h2> Hi, my name is Trayden. </h2>
-                <div className="prompt">
-                    <p>
-                        A software developer with a passion for artificial intelligence and machine learning.
-                    </p>
-                    <a href="https://www.linkedin.com/in/traydenboucher/" target="_blank" rel="noreferrer">
-                        <LinkedInIcon />
-                    </a>
-                    <a href="https://github.com/traydenpoke" target="_blank" rel="noreferrer">
-                        <GitHubIcon />
-                    </a>
-                    <a href="mailto:trayden.boucher03@gmail.com">
-                        <EmailIcon />
-                    </a>
-                </div>
-            </div>
-            <div className="skills">
-                <h1>Skills</h1>
-                <ol className="list">
-                    <li className="item">
-                        <h2> Languages </h2>
-                        <span>
-                            Java, Python, C/C++, HTML/CSS, JavaScript, SQL, Bash
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2> Technologies </h2>
-                        <span>
-                            React, Git, MySQL, SQLite
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2 > Developer Tools </h2>
-                        <span>
-                            GitHub, Visual Studio
-                        </span>
-                    </li>
-                </ol>
-            </div>
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, my name is Trayden. </h2>
+        <div className="prompt">
+          <p>
+            A software developer with a passion for artificial intelligence and machine learning.
+          </p>
+          <a href="https://www.linkedin.com/in/traydenboucher/" target="_blank" rel="noreferrer">
+            <LinkedInIcon />
+          </a>
+          <a href="https://github.com/traydenpoke" target="_blank" rel="noreferrer">
+            <GitHubIcon />
+          </a>
+          <a href="mailto:trayden.boucher03@gmail.com">
+            <EmailIcon />
+          </a>
         </div>
-    );
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Languages </h2>
+            <span>
+              Java, Python, C/C++, TypeScript, JavaScript, HTML/CSS, PHP, SQL, Bash
+            </span>
+          </li>
+          <li className="item">
+            <h2> Frameworks & Libraries </h2>
+            <span>
+              Express.js, React, jQuery, Bootstrap
+            </span>
+          </li>
+          <li className="item">
+            <h2> Technologies </h2>
+            <span>
+              Node.js, REST APIs, MongoDB, MySQL, SQLite, Linux
+            </span>
+          </li>
+          <li className="item">
+            <h2 > Developer Tools </h2>
+            <span>
+              Git, GitHub, Visual Studio, CI/CD
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
